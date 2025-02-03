bedf3b702fb2e0aabfa075ae559af6297901f2c4
Olesya Vetoshkina <our_post-office@mail.ru>
2025-02-03 07:08:36 +0300

    readme correct commit

--- src/data/stores/useStore.ts
+++ src/data/stores/useStore.ts
@@ -1,19 +1,19 @@
-// import create from "zustand";
 import create, { State, StateCreator } from "zustand";
 import { devtools } from "zustand/middleware";
 import { Store } from "../interfaces.tsx";
 import { data } from "../data.ts";
 
-function isToDoStore(object: any): object is Store {
+function isStore(object: any): object is Store {
   return "items" in object;
 }
 
+// обновление store в localStorag
 const localStorageUpdate =
   <T extends State>(config: StateCreator<T>): StateCreator<T> =>
   (set, get, api) =>
     config(
       (nextState, ...args) => {
-        if (isToDoStore(nextState)) {
+        if (isStore(nextState)) {
           window.localStorage.setItem("items", JSON.stringify(nextState.items));
         }
         set(nextState, ...args);
@@ -22,77 +22,49 @@ const localStorageUpdate =
       api
     );
 
-    const getCurrentState = () => {
-      try {
-        const currentState = JSON.parse(
-          window.localStorage.getItem("items") || data
-        );
-        return currentState;
-      } catch (err) {
-        window.localStorage.setItem("items", data);
-      }
-      return data;
-    };
+// получаем сохранённый store из localStorag
+const getCurrentState = () => {
+  try {
+    const currentState = JSON.parse(
+      window.localStorage.getItem("items") || data
+    );
+    return currentState;
+  } catch (err) {
+    window.localStorage.setItem("items", data);
+  }
+  return data;
+};
 
 // создаём store
-// export const useStore = create<Store>(
-//   devtools((set, get) => ({
-//     items: data, // массив элементов
-//     increment: (id: number) => {
-//       // увеличение счётчика
-//       const { items } = get();
-//       // запуска лоадинга
-//       set({
-//         items: items.map((item) => ({
-//           ...item,
-//           loading: item.id === id ? true : false,
-//         })),
-//       });
-//       // увеличение счётчика с задержкой 0,5ms + стоп лоадинга
-//       setTimeout(() => {
-//         set({
-//           items: items.map((item) => ({
-//             ...item,
-//             counter: item.id === id ? item.counter + 1 : item.counter,
-//             loading: item.id === id ? false : false,
-//           })),
-//         });
-//       }, 500);
-//     },
-//   }))
-// );
-
-
 export const useStore = create<Store>(
   localStorageUpdate(
     devtools((set, get) => ({
-    items: getCurrentState(), // массив элементов
-    modal: false,
-    increment: (id: number) => {
-      // увеличение счётчика
-      const { items } = get();      
-      // запуска лоадинга
-      set({
-        items: items.map((item) => ({
-          ...item,
-          loading: item.id === id ? true : false,
-        })),
-      });  
-      set({
-        modal: true
-      });     
-      // увеличение счётчика с задержкой 0,5ms + стоп лоадинга
-      setTimeout(() => {
+      items: getCurrentState(), // массив элементов
+      modal: false, // статус модального окна
+      increment: (id: number) => {
+        // увеличение счётчика
+        const { items } = get();
+        // запуска лоадинга
         set({
           items: items.map((item) => ({
             ...item,
-            counter: item.id === id ? item.counter + 1 : item.counter,
-            loading: item.id === id ? false : false,
+            loading: item.id === id ? true : false,
           })),
         });
-      }, 500);
-    },
-    
+        set({
+          modal: true,
+        });
+        // увеличение счётчика с задержкой 0,5ms + стоп лоадинга
+        setTimeout(() => {
+          set({
+            items: items.map((item) => ({
+              ...item,
+              counter: item.id === id ? item.counter + 1 : item.counter,
+              loading: item.id === id ? false : false,
+            })),
+          });
+        }, 500);
+      },
     }))
   )
 );
